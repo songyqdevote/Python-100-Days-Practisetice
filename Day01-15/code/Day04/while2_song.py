38e8c28caf6ed21f58c74e9e541713a2e7813d71
songyqdevote <songyqdevote@163.com>
2020-08-30 14:58:24 +0800

    day04

---
+++ Day01-15/code/Day04/while2_song.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :while2_song.py
+# @Time      :2020/8/30 14:54
+# @Author    :宋业强
+
+
+if __name__ == "__main__":
+    sum, n = 0, 0
+    while n < 100:
+        sum += n
+        n += 2
+    print(sum)
